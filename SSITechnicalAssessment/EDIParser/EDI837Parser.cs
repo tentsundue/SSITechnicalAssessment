6f274acb18fb0ae947b872de20c3ff9c624d7b40
Tenzin Tsundue <97130706+tentsundue@users.noreply.github.com>
2026-09-05 00:12:01 -0700

    Refactor: Moved Claim object building into its own private helper method

--- SSITechnicalAssessment/EDIParser/EDI837Parser.cs
+++ SSITechnicalAssessment/EDIParser/EDI837Parser.cs
@@ -11,7 +11,33 @@ namespace SSITechnicalAssessment.EDIParser
         const char ElementDelimiter = '*';
         const char CompositeElementDelimiter = ':';
 
+        Dictionary<string, List<string>> segmentGroups = new Dictionary<string, List<string>>();
+        
         public EDI837Parser() { }
+
+        private Claim _BuildClaim(string[] elements)
+        {
+            string patientAccountNum = elements[1];
+            decimal claimChargeAmt = decimal.Parse(elements[2]);
+
+            string clm03 = elements[3]; // unused but still kept for future cases
+            string clm04 = elements[4]; // unused but still kept for future cases
+
+            string[] facilityInfo = elements[5].Split(CompositeElementDelimiter);
+            string facilityTypeCode = facilityInfo[0];
+            char facilityCodeQual = facilityInfo[1][0];
+            string claimFreqCode = facilityInfo[2];
+
+            char signatureIndicator = elements[6][0];
+            char participationCode = elements[7][0];
+            char benefitsAsmntCertIndicator = elements[8][0];
+            char ReleaseInfoIndicator = elements[9][0];
+
+            Facility facility = new Facility(facilityTypeCode, facilityCodeQual, claimFreqCode);
+            Claim claim = new Claim(patientAccountNum, claimChargeAmt, facility, signatureIndicator, participationCode, benefitsAsmntCertIndicator, ReleaseInfoIndicator);
+
+            return claim;
+        }
 
         public List<Claim> ParseClaims(string path)
         {
@@ -33,38 +59,21 @@ namespace SSITechnicalAssessment.EDIParser
                 string segmentID = elements[0];
 
                 bool isCLM = string.Equals(segmentID, "CLM");
-                if (!isCLM)
+                if (isCLM)
                 {
-                    continue;
+                    try
+                    {
+                        Claim claim = _BuildClaim(elements);
+                        claims.Add(claim);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error reading claim segment:\n{e}");
+                        continue;
+                    }
                 }
 
-                try
-                {
-                    string patientAccountNum = elements[1];
-                    decimal claimChargeAmt = decimal.Parse(elements[2]);
-
-                    string clm03 = elements[3]; // unused but still kept for future cases
-                    string clm04 = elements[4]; // unused but still kept for future cases
-
-                    string[] facilityInfo = elements[5].Split(CompositeElementDelimiter);
-                    string facilityTypeCode = facilityInfo[0];
-                    char facilityCodeQual = facilityInfo[1][0];
-                    string claimFreqCode = facilityInfo[2];
-
-                    char signatureIndicator = elements[6][0];
-                    char participationCode = elements[7][0];
-                    char benefitsAsmntCertIndicator = elements[8][0];
-                    char ReleaseInfoIndicator = elements[9][0];
-
-                    Facility facility = new Facility(facilityTypeCode, facilityCodeQual, claimFreqCode);
-                    Claim claim = new Claim(patientAccountNum, claimChargeAmt, facility, signatureIndicator, participationCode, benefitsAsmntCertIndicator, ReleaseInfoIndicator);
-                        
-                    claims.Add(claim);
-                } catch (Exception e)
-                {
-                    Console.WriteLine($"Error reading claim segment:\n{e}");
-                    continue;
-                }
+                
                 
             }
 
