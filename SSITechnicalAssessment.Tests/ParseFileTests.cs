0ff0ce1e28a146f7c0883e2817465dac78aab625
Tenzin Tsundue <97130706+tentsundue@users.noreply.github.com>
2026-09-05 01:46:40 -0700

    Feat: Added unit tests for parsing verification

---
+++ SSITechnicalAssessment.Tests/ParseFileTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using SSITechnicalAssessment.EDIParser;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SSITechnicalAssessment.Tests
+{
+    public class ParseFileTests
+    {
+        [Fact]
+        public void ParseFile_NoFileGiven_ShouldThrowFileNotFoundException()
+        {
+            string nonExistentPath = "EDI/Path";
+
+            Assert.Throws<FileNotFoundException>(() => EDI837Parser.ParseFile(nonExistentPath));
+        }
+    }
+}
---
+++ SSITechnicalAssessment.Tests/ParseSegmentsTests.cs
@@ -0,0 +1,93 @@
+using System;
+using Xunit;
+using SSITechnicalAssessment.EDIParser;
+using System.Collections.Generic;
+using SSITechnicalAssessment.Models.ClaimModel;
+using System.IO;
+
+namespace SSITechnicalAssessment.Tests
+{
+    public class ParseSegmentsTests
+    {
+        [Fact]
+        public void ParseSegments_SingleClaimSegment_ShouldReturnSingleClaim()
+        {
+            string CLMSegment = "CLM*XYZ123000456*25***01:B:1*N*C*Y*Y~";
+
+            // Ensure there is only one claim first
+            List<Claim> claims = EDI837Parser.ParseSegments(CLMSegment);
+            Assert.Single(claims);
+
+            // Validate claim values
+            Claim claim = claims[0];
+            Assert.Equal("XYZ123000456", claim.PatientAccountNum);
+            Assert.Equal(25m, claim.ClaimChargeAmt);
+            Assert.Equal("01", claim.Facility.FacilityTypeCode);
+            Assert.Equal('B', claim.Facility.FacilityCodeQual);
+            Assert.Equal("1", claim.Facility.ClaimFreqCode);
+            Assert.Equal('N', claim.SignatureIndicator);
+            Assert.Equal('C', claim.ParticipationCode);
+            Assert.Equal('Y', claim.BenefitsAsmntCertIndicator);
+            Assert.Equal('Y', claim.ReleaseInfoIndicator);
+        }
+
+        [Fact]
+        public void ParseSegments_NoSegments_ShouldReturnNoClaims()
+        {
+            string CLMSegment = "";
+
+            List<Claim> claims = EDI837Parser.ParseSegments(CLMSegment);
+            Assert.Empty(claims);
+        }
+
+        [Fact]
+        public void ParseSegments_MultipleClaimSegments_ShouldReturnAllClaims()
+        {
+            string CLMSegments = "CLM*XYZ123000456*25***01:B:1*N*C*Y*Y~CLM*XYZ123000567*10***01:B:1*N*C*Y*Y~";
+
+            List<Claim> claims = EDI837Parser.ParseSegments(CLMSegments);
+            Assert.Equal(2, claims.Count);
+
+            Claim claim1 = claims[0];
+            Claim claim2 = claims[1];
+            Assert.Equal("XYZ123000456", claim1.PatientAccountNum);
+            Assert.Equal("XYZ123000567", claim2.PatientAccountNum);
+        }
+
+        [Fact]
+        public void ParseSegments_MultipleClaimSegments_ShouldReturnCombinedChargeAmount()
+        {
+            string CLMSegment = "CLM*XYZ123000456*25***01:B:1*N*C*Y*Y~CLM*XYZ123000567*10***01:B:1*N*C*Y*Y~";
+
+            List<Claim> claims = EDI837Parser.ParseSegments(CLMSegment);
+            Claim claim1 = claims[0];
+            Claim claim2 = claims[1];
+
+            decimal total = EDI837Parser.GetTotalChargeAmtAllClaims(claims);
+            decimal expected = 35;
+            Assert.Equal(total, expected);
+
+        }
+
+        [Fact]
+        public void ParseSegments_MultipleSegmentIdentifiers_ShouldIgnoreNonClaimSegments()
+        {
+            string NonCLMSegment = "GS*HC*SSIGROUP*SSIGROUP*20220120*1544*1*X*005010X222A1~CLM*XYZ123000456*25***01:B:1*N*C*Y*Y~";
+
+            List<Claim> claims = EDI837Parser.ParseSegments(NonCLMSegment);
+            Assert.Single(claims);
+
+            Claim claim = claims[0];
+            Assert.Equal("XYZ123000456", claim.PatientAccountNum);
+        }
+
+        [Fact]
+        public void ParseSegments_MalformedCLMSegment_ShouldSkipAndReturnNoClaims()
+        {
+            string NonCLMSegment = "CLM* ACC001*NOTANUMBER * **01:B: 1 * Y * A * Y * Y~";
+
+            List<Claim> claims = EDI837Parser.ParseSegments(NonCLMSegment);
+            Assert.Empty(claims);
+        }
+    }
+}
